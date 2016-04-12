7c827b05899534853312efe706ad5dee2c5e68aa
huyhuynh408 <huyhuynh85@gmail.com>
2016-04-11 20:27:34 -0500

    got basic form and posts to show with upvoting

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 var app = angular.module('clone', []);
 
 app.controller('mainCtrl', ['$scope', function($scope){
-  $scope.test = 'Hello'
+  $scope.posts = [
+    {title: 'post 1', upvotes: 5},
+    {title: 'post 2', upvotes: 52},
+    {title: 'post 3', upvotes: 3},
+    {title: 'post 4', upvotes: 4},
+  ];
+
+  $scope.addPost = function(){
+    if(!$scope.title || $scope.title === '') { return; }
+    $scope.posts.push({
+      title:   $scope.title,
+      link:    $scope.link,
+      upvotes: 0
+    });
+    $scope.title = '';
+    $scope.link  = '';
+  };
+
+  $scope.incrementUpvotes = function(post){
+    post.upvotes += 1;
+  };
 }]);
