0d7df69e8b809d256ed4e20b4b70832725a96d9a
huyhuynh408 <huyhuynh85@gmail.com>
2016-04-11 20:40:10 -0500

    added posts service

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var app = angular.module('clone', []);
 
-app.controller('mainCtrl', ['$scope', function($scope){
-  $scope.posts = [
-    {title: 'post 1', upvotes: 5},
-    {title: 'post 2', upvotes: 52},
-    {title: 'post 3', upvotes: 3},
-    {title: 'post 4', upvotes: 4},
-  ];
+app.factory('posts', [function(){
+  var object = {
+    posts: []
+  };
+  return object;
+}]);
+
+app.controller('mainCtrl', ['$scope', 'posts', function($scope, posts){
+  $scope.posts = posts.posts
 
   $scope.addPost = function(){
     if(!$scope.title || $scope.title === '') { return; }
