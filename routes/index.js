1819bf9f711cae2603579898d936cfc302c8f320
huyhuynh408 <huyhuynh85@gmail.com>
2016-04-13 20:02:43 -0500

    added more routes for post

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+
+  query.exec(function(err, post) {
+    if (err) { return next(err); }
+    if (!post) { return next(new Error('Cannot find post')); }
+
+    req.post = post;
+    return next();
+  });
+});
+
 router.get('/posts', function(req, res, next) {
   Post.find(function(err, posts) {
     if (err) { return next(err); }
@@ -24,6 +36,10 @@ router.post('/posts', function(req, res, next) {
 
     res.json(post);
   });
+});
+
+router.get('/posts/:post', function(req, res) {
+  res.json(req.post);
 });
 
 module.exports = router;
