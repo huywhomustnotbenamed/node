d9fe2b5c67ea49bb39c591d48aa7103f68195237
huyhuynh408 <huyhuynh85@gmail.com>
2016-04-14 17:24:53 -0500

    got routes to work

--- routes/index.js
+++ routes/index.js
@@ -8,18 +8,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.param('post', function(req, res, next, id) {
-  var query = Post.findById(id);
-
-  query.exec(function(err, post) {
-    if (err) { return next(err); }
-    if (!post) { return next(new Error('Cannot find post')); }
-
-    req.post = post;
-    return next();
-  });
-});
-
 router.get('/posts', function(req, res, next) {
   Post.find(function(err, posts) {
     if (err) { return next(err); }
@@ -40,6 +28,26 @@ router.post('/posts', function(req, res, next) {
 
 router.get('/posts/:post', function(req, res) {
   res.json(req.post);
+});
+
+router.put('/posts/:post/upvote', function(req, res, next) {
+  req.post.upvote(function(err, post) {
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
+});
+
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+
+  query.exec(function (err, post){
+    if (err) { return next(err); }
+    if (!post) { return next(new Error('can\'t find post')); }
+
+    req.post = post;
+    return next();
+  });
 });
 
 module.exports = router;
